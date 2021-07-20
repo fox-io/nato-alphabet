83667478ea2fa318432bdc9d1a5914add933b589
fox-io <gitlab.fox-io@foxdata.io>
2021-07-20 10:54:24 -0500

    Solution without pandas.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nato-alphabet
 Convert words to nato phonetic alphabet
+
+Day 26
+https://www.udemy.com/course/100-days-of-code/learn/lecture/20763688#content
--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-import pandas
+import csv
 
 
 class NATOPhoneticAlphabet:
     """Class which performs prompts and converts a string to the NATO Phonetic Alphabet."""
     def __init__(self):
-        self.db = {
-            "data_file_name": "./nato_phonetic_alphabet.csv",
-            "nato_phonetic_alphabet": pandas.DataFrame(),
-            "word_to_convert": "",
-        }
+        self.file_name = "./nato_phonetic_alphabet.csv"
+        self.nato_alphabet = {}
+        self.user_input = ""
 
     def import_data(self):
         """Imports phonetic alphabet data from CSV file."""
-        self.db["nato_phonetic_alphabet"] = pandas.read_csv(self.db["data_file_name"])
+        with open(self.file_name) as csv_file:
+            csv_data = csv.reader(csv_file)
+            for row in csv_data:
+                if row[0] != "letter":
+                    self.nato_alphabet[row[0]] = row[1]
 
     def get_user_input(self):
         """Prompts user for a word to convert."""
-        self.db["word_to_convert"] = input("Enter a word to convert: ")
+        self.user_input = input("Enter a word to convert: ")
+        pass
 
     def convert_string(self):
         """Accepts a string and returns a string containing the phonetic version."""
-        letters_in_word = list(self.db["word_to_convert"])
+        letters_in_word = list(self.user_input)
         converted_letters = []
         for each_letter in letters_in_word:
-            result = self.db["nato_phonetic_alphabet"].query(f'letter == "{each_letter.upper()}"')
-            converted_letters.append(result.values[0][1])
+            converted_letters.append(self.nato_alphabet[each_letter.upper()])
         print(converted_letters)
+        pass
 
 
 if __name__ == "__main__":
