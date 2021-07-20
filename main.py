da2a2feb0560150b37c2178db80c40aa7522b55a
fox-io <gitlab.fox-io@foxdata.io>
2021-07-20 04:16:48 -0500

    Implemented all functionality.

--- main.py
+++ main.py
@@ -6,17 +6,13 @@ class NATOPhoneticAlphabet:
     def __init__(self):
         self.db = {
             "data_file_name": "./nato_phonetic_alphabet.csv",
-            "nato_phonetic_alphabet": {},
+            "nato_phonetic_alphabet": pandas.DataFrame(),
             "word_to_convert": "",
         }
 
-        self.import_data()
-        self.get_user_input()
-        self.convert_string()
-
     def import_data(self):
         """Imports phonetic alphabet data from CSV file."""
-        pass
+        self.db["nato_phonetic_alphabet"] = pandas.read_csv(self.db["data_file_name"])
 
     def get_user_input(self):
         """Prompts user for a word to convert."""
@@ -24,8 +20,16 @@ class NATOPhoneticAlphabet:
 
     def convert_string(self):
         """Accepts a string and returns a string containing the phonetic version."""
-        print(self.db["word_to_convert"])
+        letters_in_word = list(self.db["word_to_convert"])
+        converted_letters = []
+        for each_letter in letters_in_word:
+            result = self.db["nato_phonetic_alphabet"].query(f'letter == "{each_letter.upper()}"')
+            converted_letters.append(result.values[0][1])
+        print(converted_letters)
 
 
 if __name__ == "__main__":
     app = NATOPhoneticAlphabet()
+    app.import_data()
+    app.get_user_input()
+    app.convert_string()
